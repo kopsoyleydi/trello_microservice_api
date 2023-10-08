d923c6622bd73b0e3c27eb76599e2d3b8b941965
kopsoyleydi <kuralbaibexultan@gmail.com>
2023-10-08 21:53:34 +0600

    AWS Configuration

--- user_service/src/main/java/com/example/user_service/bucket/FakeS3.java
+++ user_service/src/main/java/com/example/user_service/bucket/FakeS3.java
@@ -1,4 +1,76 @@
 package com.example.user_service.bucket;
 
-public class FakeS3 {
+import org.apache.commons.io.FileUtils;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.utils.IoUtils;
+
+import java.io.*;
+
+public class FakeS3 implements S3Client {
+    // TODO: Check if this works for Windows users
+    private static final String PATH =
+            System.getProperty("user.home") + "/.omyrz/s3";
+
+    @Override
+    public String serviceName() {
+        return "fake";
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public PutObjectResponse putObject(PutObjectRequest putObjectRequest,
+                                       RequestBody requestBody)
+            throws AwsServiceException, SdkClientException {
+        InputStream inputStream = requestBody.contentStreamProvider().newStream();
+
+        try {
+            byte[] bytes = IoUtils.toByteArray(inputStream);
+            FileUtils.writeByteArrayToFile(
+                    new File(
+                            buildObjectFullPath(
+                                    putObjectRequest.bucket(),
+                                    putObjectRequest.key())
+                    ),bytes
+            );
+            return PutObjectResponse.builder().build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public ResponseInputStream<GetObjectResponse> getObject(
+            GetObjectRequest getObjectRequest)
+            throws  AwsServiceException, SdkClientException {
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(
+                    buildObjectFullPath(
+                            getObjectRequest.bucket(),
+                            getObjectRequest.key())
+            );
+            return new ResponseInputStream<>(
+                    GetObjectResponse.builder().build(),
+                    fileInputStream
+            );
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String buildObjectFullPath(String bucketName, String key) {
+        return PATH + "/" + bucketName + "/" + key;
+    }
 }
