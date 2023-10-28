314d0d8a2c2549caa3c31088f42ba6a193330799
kopsoyleydi <kuralbaibexultan@gmail.com>
2023-10-28 21:23:44 +0600

    application.properties config

--- api_gateway/src/main/java/com/example/api_gateway/service/JwtUtil.java
+++ api_gateway/src/main/java/com/example/api_gateway/service/JwtUtil.java
@@ -2,8 +2,8 @@ package com.example.api_gateway.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
+
+
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,12 +11,11 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.*;
-import java.util.function.Function;
 
 @Service
 public class JwtUtil {
 
-    @Value("${jwt.secret}")
+    @Value("${application.security.jwt.secret-key}")
     private String secret;
 
     private Key key;
@@ -26,8 +25,32 @@ public class JwtUtil {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    private Claims extractAllClaims(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            claims = null;
+        }
+        return claims;
+    }
+
     public Claims getClaims(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public String extractUsername(String token) {
+        String username;
+        try {
+            final Claims claims = this.extractAllClaims(token);
+            username = claims.getSubject();
+        } catch (Exception e) {
+            username = null;
+        }
+        return username;
     }
 
     public boolean isExpired(String token) {
