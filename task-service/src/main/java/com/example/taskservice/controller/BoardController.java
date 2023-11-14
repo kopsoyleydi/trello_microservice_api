2e7e45f162040bdac2c1fd1fa759950292f367be
kopsoyleydi <kuralbaibexultan@gmail.com>
2023-11-14 14:37:12 +0600

    fixing

--- task-service/src/main/java/com/example/taskservice/controller/BoardController.java
+++ task-service/src/main/java/com/example/taskservice/controller/BoardController.java
@@ -4,12 +4,10 @@ import com.example.taskservice.bodies.AddBoard;
 import com.example.taskservice.dto.BoardDto;
 import com.example.taskservice.dto.response.CommonResponse;
 import com.example.taskservice.service.boardservice.BoardServiceInter;
-import com.example.taskservice.service.boardservice.impl.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +19,10 @@ public class BoardController {
 
     @PostMapping(value = "/addBoard")
     public CommonResponse addBoardService(@RequestBody AddBoard addBoard){
+        BoardDto boardDto = boardServiceInter.addBoard(addBoard);
         return CommonResponse.builder()
                 .message("Board added success")
-                .data(boardServiceInter.addBoard(addBoard))
+                .data(boardDto)
                 .status(HttpStatus.CREATED)
                 .build();
     }
