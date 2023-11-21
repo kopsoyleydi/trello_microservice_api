1cd065faa6d360b52a09ade0d060d3742bb5b039
kopsoyleydi <kuralbaibexultan@gmail.com>
2023-11-21 14:35:16 +0600

    adding sheets api on grade service

--- grade-service/src/main/java/com/example/gradeservice/controller/FormulaController.java
+++ grade-service/src/main/java/com/example/gradeservice/controller/FormulaController.java
@@ -4,6 +4,7 @@ package com.example.gradeservice.controller;
 import com.example.gradeservice.config.GoogleSheetsConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ import java.security.GeneralSecurityException;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/api/grade")
 @RequiredArgsConstructor
 public class FormulaController {
 
