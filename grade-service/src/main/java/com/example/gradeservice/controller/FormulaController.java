802b13a9762d38d6f1ec85b939a43a39d0b29972
kopsoyleydi <kuralbaibexultan@gmail.com>
2023-11-21 14:24:39 +0600

    adding sheets api on grade service

--- grade-service/src/main/java/com/example/gradeservice/controller/FormulaController.java
+++ grade-service/src/main/java/com/example/gradeservice/controller/FormulaController.java
@@ -1,8 +1,24 @@
 package com.example.gradeservice.controller;
 
 
+import com.example.gradeservice.config.GoogleSheetsConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class FormulaController {
+
+    private final GoogleSheetsConfig googleSheetsConfig;
+
+
+    @GetMapping("/getSheetsInfo")
+    private List<Object> getInfoFromSheet() throws GeneralSecurityException, IOException {
+        return googleSheetsConfig.getInfoFromSheet();
+    }
 }
---
+++ grade-service/src/main/java/com/example/gradeservice/service/impl/CreateSheet.java
@@ -0,0 +1,9 @@
+package com.example.gradeservice.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateSheet {
+
+
+}
